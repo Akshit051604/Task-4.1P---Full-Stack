fb5acc53c27a633774866ad0e127fb6222a61df1
Akshit051604 <akshitbaren0516@gmail.com>
2024-09-23 10:45:53 +0530

    Update and rename Navbar.jsx to Tutorial.jsx

---
+++ Tutorial.jsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import { Divider, Image, CardHeader, CardDescription, CardContent, Card, Icon, CardGroup, Container, Button } from 'semantic-ui-react';
+import tutorial1 from './Images/tutotial1.jpg';
+import tutorial2 from './Images/tutorial2.jpg';
+import tutorial3 from './Images/tutorial3.jpg';
+const Tutorial = () => {
+    return (
+        <Container>
+            <Divider />
+            <h2 style={{ textAlign: 'center', color: 'Grey', fontWeight: 'bold' }}> Demon Slayer | Hashiras </h2>
+            <Divider />
+            <CardGroup centered>
+                <Card color='green'>
+                    <Image src={tutorial1} wrapped ui={false} />
+                    <CardContent>
+                        <CardHeader> Kyojuro Rangoku </CardHeader>
+                        <CardDescription>
+                            He is a Flame Hashira.
+                            Using Flame Breathing Style!
+                        </CardDescription>
+                    </CardContent>
+                    <CardContent extra>
+                        <a>
+                            <Icon name='star' />
+                            5.0
+                        </a>
+                    </CardContent>
+                    <CardContent extra>
+                        <a>
+                            <Icon name='user' />
+                            Koyoharu Gotouge
+                        </a>
+                    </CardContent>
+                </Card>
+                <Card color='blue'>
+                    <Image src={tutorial2} wrapped ui={false} />
+                    <CardContent>
+                        <CardHeader> Gyomei Himejima </CardHeader>
+                        <CardDescription>
+                            He is a Stone Hashira.
+                            Strongest Hashira currently!
+                        </CardDescription>
+                    </CardContent>
+                    <CardContent extra>
+                        <a>
+                            <Icon name='star' />
+                            5.0
+                        </a>
+                    </CardContent>
+                    <CardContent extra>
+                        <a>
+                            <Icon name='user' />
+                            Koyoharu Gotouge
+                        </a>
+                    </CardContent>
+                </Card>
+                <Card color='red'>
+                    <Image src={tutorial3} wrapped ui={false} />
+                    <CardContent>
+                        <CardHeader> Tomioka Giyu </CardHeader>
+                        <CardDescription>
+                            He is a Water Hashira.
+                        </CardDescription>
+                    </CardContent>
+                    <CardContent extra>
+                        <a>
+                            <Icon name='star' />
+                            4.9
+                        </a>
+                    </CardContent>
+                    <CardContent extra>
+                        <a>
+                            <Icon name='user' />
+                            Koyoharu Gotouge
+                        </a>
+                    </CardContent>
+                </Card>
+            </CardGroup>
+            <Button color='purple' style={{ marginTop: '20px' }}>More Hashiras</Button>
+        </Container>
+    )
+}
+
+export default Tutorial
